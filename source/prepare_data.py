31cb7ea3667a059a5f2b0017f80e91889fac77d7
hung.ai <hung.dhk@hahalolo.com>
2023-06-16 17:43:45 +0700

    update

--- source/prepare_data.py
+++ source/prepare_data.py
@@ -1,8 +1,9 @@
 import cv2
-from facenet_pytorch import MTCNN
+from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 import os
 import numpy as np
+from utils import prepare_data_for_training, prepare_data_for_testing
 
 # Change base dir for our directory
 BASE_DIR = '/content/drive/MyDrive/projects/ComputerVision/identifier_system_by_face'
@@ -17,6 +18,19 @@ EMBEDDING_FOLDER = 'embeddings'
 # Check type of device (cpu, gpu, tpu)
 device =  torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
-mtcnn = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = device)
 
-# Extract face from input image
+# Initalize face detector - MTCNN
+face_detector = MTCNN(thresholds= [0.7, 0.7, 0.8] ,keep_all=True, device = device)
+
+# Initialize face embedder
+# Intialize embedding model
+face_embedder = InceptionResnetV1(
+    classify=False,
+    pretrained="vggface2"
+).to(device)
+face_embedder.eval()
+
+if __name__ == '__main__':
+    # Extract face from input image
+    prepare_data_for_training(face_detector, face_embedder, device)
+    prepare_data_for_testing(face_detector, face_embedder, device)
--- source/train.py
+++ source/train.py
@@ -1,8 +1,37 @@
-import cv2
-from facenet_pytorch import MTCNN
-import torch
-import os
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Lambda, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import VGG16
+import tensorflow_addons as tfa
 
 #check type of device (cpu, gpu, tpu)
-device =  torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(device)
+def _base_network():
+  model = VGG16(include_top = True, weights = None)
+  dense = Dense(128)(model.layers[-4].output)
+  norm2 = Lambda(lambda x: tf.math.l2_normalize(x, axis = 1))(dense)
+  model = Model(inputs = [model.input], outputs = [norm2])
+  return model
+
+model = _base_network()
+model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tfa.losses.TripletSemiHardLoss())
+
+X_train, testing_faces, y_train, testing_labels = load_dataset_for_recognition()
+gen_train = tf.data.Dataset.from_tensor_slices((X_train, y_train)).repeat().shuffle(1024).batch(32)
+
+history = model.fit(
+    gen_train,
+    steps_per_epoch = 50,
+    epochs=5)
+
+MODEL_DIR = os.path.join(BASE_DIR, 'model')
+if os.path.exists(MODEL_DIR) != True:
+    os.mkdir(MODEL_DIR)
+model_name = 'face_recognition_triplot.h5'
+model_path = os.path.join(MODEL_DIR, model_name)
+model.save(model_path)
